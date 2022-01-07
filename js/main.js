888c174b45f5383a93bc5c5c97623c8c0b9c624a
shehster shehster <shehs6767@mail.ru>
2022-01-07 15:49:38 +0300

    added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let burgerBlock = document.querySelector('.burger');
+let line1 = document.querySelector('.burger__line-1');
+let line2 = document.querySelector('.burger__line-2');
+let line3 = document.querySelector('.burger__line-3');
+let burgerMenu = document.querySelector('.burger__menu');
+let body = document.querySelector('body');
+
+const menuBurger = () => {
+    burgerMenu.classList.toggle('visible');
+    body.classList.toggle('body__scroll')
+}
+
+
+
+
+burgerBlock.addEventListener('click', () => {
+    line1.classList.toggle('rotate1');
+    line2.classList.toggle("hidden");
+    line3.classList.toggle('rotate2');
+    menuBurger();
+})
